260c31939f4f0f56edfc68a39716c0e13d362fd9
Cameron Bannasch <cameron.bannasch@gmail.com>
2024-10-09 14:09:51 -0700

    Run linter

--- Tests/radio_test.py
+++ Tests/radio_test.py
@@ -117,7 +117,8 @@ def client(passcode):
     cubesat.radio1.node = 0xFA
     cubesat.radio1.destination = 0xFB
 
-    print('''
+    print(
+        """
     =============== /\\ ===============
     = Please select command  :)      =
     ==================================
@@ -129,7 +130,8 @@ def client(passcode):
     6 - joke_reply                   |
     7 - FSK                          |
     ==================================
-    ''')
+    """
+    )
 
     chosen_command = input("Select cmd pls: ")
 
@@ -140,23 +142,29 @@ def client(passcode):
     elif chosen_command == "2":
         packet = b"\x00\x00\x00\x00" + passcode.encode() + b"\xd4\x9f"
     elif chosen_command == "3":
-        packet = b"\x00\x00\x00\x00" + passcode.encode() + b"\x12\x06" + b"\x0b\xfdI\xec"
+        packet = (
+            b"\x00\x00\x00\x00" + passcode.encode() + b"\x12\x06" + b"\x0b\xfdI\xec"
+        )
     elif chosen_command == "4":
         packet = b"\x00\x00\x00\x00" + passcode.encode() + b"8\x93" + input("Query: ")
     elif chosen_command == "5":
-        packet = b"\x00\x00\x00\x00" + passcode.encode() + b"\x96\xa2" + input("Command: ")
+        packet = (
+            b"\x00\x00\x00\x00" + passcode.encode() + b"\x96\xa2" + input("Command: ")
+        )
     elif chosen_command == "6":
         packet = b"\x00\x00\x00\x00" + passcode.encode() + b"\xa5\xb4"
     elif chosen_command == "7":
         packet = b"\x00\x00\x00\x00" + passcode.encode() + b"\x56\xc4"
     else:
-        print("Command is not valid or not implemented open radio_test.py and add them yourself!")
+        print(
+            "Command is not valid or not implemented open radio_test.py and add them yourself!"
+        )
 
     tries = 0
     while True:
         sleep(0.1)
         tries += 1
-        if (tries > 5):
+        if tries > 5:
             print("We tried 5 times! And there was no response. Quitting.")
             return
         cubesat.radio1.send(packet)
@@ -222,12 +230,13 @@ print(
 """
 )
 
-passcode=""
+passcode = ""
 if device_selection == "C":
-    passcode = input("What's the passcode (in plain text, will automagically be converted to UTF-8): ")
+    passcode = input(
+        "What's the passcode (in plain text, will automagically be converted to UTF-8): "
+    )
 
 while True:
-
 
     if device_selection == "A":
         time.sleep(1)
