e6dd3e1110b09fc8cb07d76bbbacc4d6f39c5f50
blakejameson <lpo13@txstate.edu>
2025-01-05 00:37:28 -0600

    Trying to utilize adafruit_logging package. Ran into memory allocation errors but managed to get past them with the work done here. A lot more work to be done

--- lib/pysquared/functions.py
+++ lib/pysquared/functions.py
@@ -114,6 +114,7 @@ class functions:
             self.debug_print("Failed to send packet")
         del self.field
         del Field
+        gc.collect()
 
     def send_packets(self, data: Union[str, bytearray]) -> None:
         """Sends packets of data over the radio with delay between packets.
@@ -152,6 +153,7 @@ class functions:
         self.field.Beacon(lora_beacon)
         del self.field
         del Field
+        gc.collect()
 
     def joke(self) -> None:
         self.send(random.choice(self.jokes))
@@ -224,6 +226,7 @@ class functions:
             self.state_bool: bool = False
         del self.field
         del Field
+        gc.collect()
 
     def send_face(self) -> None:
         """Calls the data transmit function from the field class"""
@@ -236,6 +239,7 @@ class functions:
         )
         del self.field
         del Field
+        gc.collect()
 
     def listen(self) -> bool:
         import lib.pysquared.cdh as cdh
@@ -308,6 +312,7 @@ class functions:
 
             del a
             del Big_Data
+            gc.collect()
 
         except Exception as e:
             self.debug_print("Big_Data error" + "".join(traceback.format_exception(e)))
