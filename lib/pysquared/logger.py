990f8fdbe3726b4170b7cb5bf0d18d87a38084c0
blakejameson <lpo13@txstate.edu>
2025-01-06 18:12:32 -0600

    created base functions for the Logger

--- lib/pysquared/logger.py
+++ lib/pysquared/logger.py
@@ -7,5 +7,17 @@ class Logger:
         self.internal_logger = logging.getLogger("logger")
         self.internal_logger.setLevel(logging.DEBUG)
 
-    def log(self, message):
-        self.internal_logger.debug(message)
+    def log(self, message, *args):
+        self.internal_logger.debug(message, *args)
+
+    def info(self, message, *args):
+        self.internal_logger.info(message, *args)
+
+    def warning(self, message, *args):
+        self.internal_logger.warning(message, *args)
+
+    def error(self, message, *args):
+        self.internal_logger.error(message, *args)
+
+    def critical(self, message, *args):
+        self.internal_logger.critical(message, *args)
