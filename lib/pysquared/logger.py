67bd38e23d7155443135067d815d5ff00a646e5c
blakejameson <lpo13@txstate.edu>
2025-01-08 00:02:15 -0600

    testing out different formatting for log output. Extra code remains that will later be removed as I decide on one of the formats. still very early on in process

--- lib/pysquared/logger.py
+++ lib/pysquared/logger.py
@@ -1,4 +1,6 @@
 import adafruit_logging as logging
+import json
+import time
 
 
 class Logger:
@@ -6,10 +8,46 @@ class Logger:
     def __init__(self):
         self.internal_logger = logging.getLogger("logger")
         self.internal_logger.setLevel(logging.DEBUG)
+        print_handler = logging.StreamHandler()
+        self.internal_logger.addHandler(print_handler)
+        default_formatter = logging.Formatter()
 
-    def log(self, filename, message, *args):
+        print_handler.setFormatter(default_formatter)
+        self.internal_logger.info("Default formatter example")
+
+        timestamp_formatter = logging.Formatter(
+            fmt="%(asctime)s %(levelname)s: %(message)s"
+        )
+        print_handler.setFormatter(timestamp_formatter)
+        self.internal_logger.info("Timestamp formatter example")
+
+        custom_vals_formatter = logging.Formatter(
+            fmt="%(ip)s %(levelname)s: %(message)s", defaults={"ip": "192.168.1.188"}
+        )
+        print_handler.setFormatter(custom_vals_formatter)
+        self.internal_logger.info("Custom formatter example")
+
+        bracket_timestamp_formatter = logging.Formatter(
+            fmt="{asctime} {levelname}: {message}", style="{"
+        )
+        print_handler.setFormatter(bracket_timestamp_formatter)
+        self.internal_logger.info("Timestamp formatter bracket style example")
+
+    def debug(self, filename, message, **kwargs):
+        # logging.basicConfig(level=logging.INFO, format='%(message)s')
         full_message = "[" + filename + "]" + message
-        self.internal_logger.debug(full_message, *args)
+        now = time.localtime()
+        asctime = f"{now.tm_year}-{now.tm_mon:02d}-{now.tm_mday:02d} {now.tm_hour:02d}:{now.tm_min:02d}:{now.tm_sec:02d}"
+        kwargs["time"] = asctime
+        output = "[%s] %s >>> %s" % (filename, message, json.dumps(kwargs))
+        kwargs["message"] = message
+        kwargs["level"] = "DEBUG"
+        alternate_output = "[%s] >>> %s" % (filename, json.dumps(kwargs))
+        print(kwargs)
+        print(output)
+        print(alternate_output)
+        self.internal_logger.debug(output)
+        self.internal_logger.debug(alternate_output)
 
     def info(self, message, *args):
         self.internal_logger.info(message, *args)
