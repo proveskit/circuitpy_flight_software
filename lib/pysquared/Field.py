a5c9352e2057b312fe1c2082bca820e9c88f1aa4
blakejameson <lpo13@txstate.edu>
2025-01-14 21:38:07 -0600

    changed print statements in Field.py to be logger function calls

--- lib/pysquared/Field.py
+++ lib/pysquared/Field.py
@@ -9,6 +9,8 @@ import traceback
 from lib.pysquared.debugcolor import co
 from lib.pysquared.logger import Logger
 
+filename = "Field.py"
+
 
 class Field:
     def debug_print(self, statement):
@@ -23,20 +25,38 @@ class Field:
     def Beacon(self, msg):
         try:
             if self.cubesat.is_licensed:
-                self.debug_print("I am beaconing: " + str(msg))
-                print(
-                    "Message Success: "
-                    + str(self.cubesat.radio1.send(bytes(msg, "UTF-8")))
+                # self.debug_print("I am beaconing: " + str(msg))
+                self.logger.info(
+                    filename=filename, message="I am beaconing: " + str(msg)
+                )
+                # print(
+                # "Message Success: "
+                # + str(self.cubesat.radio1.send(bytes(msg, "UTF-8")))
+                # )
+                self.logger.info(
+                    filename=filename,
+                    message="Message Success: "
+                    + str(self.cubesat.radio1.send(bytes(msg, "UTF-8"))),
                 )
             else:
-                self.debug_print(
-                    "Please toggle licensed variable in code once you obtain an amateur radio license"
+                # self.debug_print(
+                #    "Please toggle licensed variable in code once you obtain an amateur radio license"
+                # )
+                self.logger.debug(
+                    filename=filename,
+                    message="Please toggle licensed variable in code once you obtain an amateur radio license",
                 )
         except Exception as e:
-            self.debug_print(
-                "Tried Beaconing but encountered error: ".join(
+            # self.debug_print(
+            #    "Tried Beaconing but encountered error: ".join(
+            # traceback.format_exception(e)
+            #    )
+            # )
+            self.logger.error(
+                filename=filename,
+                message="Tried Beaconing but encountered error: ".join(
                     traceback.format_exception(e)
-                )
+                ),
             )
 
     def troubleshooting(self):
@@ -44,4 +64,5 @@ class Field:
         pass
 
     def __del__(self):
-        self.debug_print("Object Destroyed!")
+        # self.debug_print("Object Destroyed!")
+        self.logger.debug(filename=filename, message="Object Destroyed!")
