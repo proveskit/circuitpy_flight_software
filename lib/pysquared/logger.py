aaeb9a74ba43a78eb9923bd916913d3eb8cbe067
blakejameson <lpo13@txstate.edu>
2025-01-18 18:51:10 -0600

    added member variable to Logger class pertaining to the logger logging to standard output or to a file. While standard output is only supported now, the log file will be implemented at some point in the future

--- lib/pysquared/logger.py
+++ lib/pysquared/logger.py
@@ -14,14 +14,21 @@ class Logger:
         # self.debugmode = DebugMode.DEBUGMODE
         self.internal_logger = logging.getLogger("logger")
         self.internal_logger.setLevel(logging.DEBUG)
+        self.logToFile = False
+        self.logToStandardOut = True
 
-    def setPrintMode(self):
-        # self.debugmode = DebugMode.PRINTMODE
-        pass
+    # @blakejameson NOTE: functionality to save logs to file will be implemented at a later point. For now, logs will
+    # be output to standard output
+    # def saveLogToFile():
+    #    pass
 
-    def setDebugMode(self):
-        # self.debugmode = DebugMode.DEBUGMODE
-        pass
+    def setLogToFile(self):
+        self.logToFile = True
+        self.logToStandardOut = False
+
+    def setLogToStdOut(self):
+        self.logToStandardOut = True
+        self.logToFile = False
 
     def debug(self, filename, **kwargs):
         now = time.localtime()
@@ -30,7 +37,9 @@ class Logger:
         kwargs["level"] = "DEBUG"
         kwargs["file"] = filename
         json_output = json.dumps(kwargs)
-        print(json_output)
+
+        if self.logToStandardOut:
+            print(json_output)
 
     def info(self, filename, **kwargs):
         now = time.localtime()
@@ -39,7 +48,9 @@ class Logger:
         kwargs["level"] = "INFO"
         kwargs["file"] = filename
         json_output = json.dumps(kwargs)
-        print(json_output)
+
+        if self.logToStandardOut:
+            print(json_output)
 
     def warning(self, filename, **kwargs):
         now = time.localtime()
@@ -48,7 +59,9 @@ class Logger:
         kwargs["level"] = "WARNING"
         kwargs["file"] = filename
         json_output = json.dumps(kwargs)
-        print(json_output)
+
+        if self.logToStandardOut:
+            print(json_output)
 
     def error(self, filename, **kwargs):
         now = time.localtime()
@@ -57,7 +70,9 @@ class Logger:
         kwargs["level"] = "ERROR"
         kwargs["file"] = filename
         json_output = json.dumps(kwargs)
-        print(json_output)
+
+        if self.logToStandardOut:
+            print(json_output)
 
     def critical(self, filename, **kwargs):
         now = time.localtime()
@@ -66,4 +81,6 @@ class Logger:
         kwargs["level"] = "CRITICAL"
         kwargs["file"] = filename
         json_output = json.dumps(kwargs)
-        print(json_output)
+
+        if self.logToStandardOut:
+            print(json_output)
