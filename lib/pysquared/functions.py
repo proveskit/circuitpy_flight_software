7ce9660053b4cca9a506ad7009518152e91420d5
daveencar03 <111798453+daveencar03@users.noreply.github.com>
2025-01-22 12:32:33 -0500

    pcb_comms include in functions lib
    Added the pcb_comms function into the functions library in the V2.0.0 proves software

--- lib/pysquared/functions.py
+++ lib/pysquared/functions.py
@@ -460,3 +460,42 @@ class functions:
 
         self.cubesat.enable_rf.value = True
         return True
+    
+    def pcb_comms(self) -> None:                
+        debug_print("Yapping to the PCB now - D")
+        
+        #await asyncio.sleep(600)
+        # Initialize communication and FCBCommunicator
+        com1 = EasyComms(board.TX, board.RX, baud_rate=9600)
+        com1.start()
+        fcb_comm = FCBCommunicator(com1)
+
+        # Start interaction loop
+        while True:
+            overhead_command = com1.overhead_read()
+
+            # Set the command
+            command = 'chunk'
+            time.sleep(2)
+
+            if command.lower() == 'chunk':
+                fcb_comm.send_command("chunk")
+                
+                if fcb_comm.wait_for_acknowledgment():
+                    jpg_bytes = fcb_comm.send_chunk_request()
+                    
+                    if jpg_bytes is not None:
+                        fcb_comm.save_image(jpg_bytes)
+                        
+            command = 'end'
+            
+            if command.lower() == 'end':
+                fcb_comm.end_communication()
+                time.sleep(3)
+                break
+
+            else:
+                print("Wrong command entered, try again.")
+            
+            gc.collect()
+            await asyncio.sleep(600)
