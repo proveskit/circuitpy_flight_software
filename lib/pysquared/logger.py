4198845712e8d4074a99469c507ed066cf17142c
blakejameson <lpo13@txstate.edu>
2025-01-28 21:35:47 -0600

    implementing enum for use in Logger class

--- lib/pysquared/logger.py
+++ lib/pysquared/logger.py
@@ -7,6 +7,8 @@ Logs can be output to standard output or saved to a file (functionality to be im
 import json
 import time
 
+from adafruit_blinka import Enum
+
 from lib.pysquared.nvm.counter import Counter
 from lib.pysquared.nvm.registers import ERRORCNT
 
@@ -16,58 +18,38 @@ except ImportError:
     pass
 
 
+class LogMode(Enum):
+    PRINT = 1
+    FILE = 2
+    BOTH = 3
+
+
+class LogLevel(Enum):
+    NOTSET = 0
+    DEBUG = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+    CRITICAL = 5
+
+
 class Logger:
     def __init__(
         self,
         datastore: ByteArray,
-        log_level: str = "DEBUG",
-        log_mode: str = "PRINT",
+        log_level: int = LogLevel.DEBUG,
+        log_mode: int = LogMode.PRINT,
     ) -> None:
         # mapping each level to a numerical value. Used to help support log_level.
         # If log function used is equal to or above the value, it can be used
         self.datastore: ByteArray = datastore
-        self.levels_map: dict = {
-            "NOTSET": 0,
-            "DEBUG": 10,
-            "INFO": 20,
-            "WARNING": 30,
-            "ERROR": 40,
-            "CRITICAL": 50,
-        }
-        self.log_modes_set: set = {"PRINT", "FILE", "BOTH"}
         self.logToStandardOut: bool = True
         self.error_count: Counter = Counter(index=ERRORCNT, datastore=self.datastore)
-        self.log_level: str = self.parse_log_level(log_level)
-        self.log_mode: str = self.parse_log_mode(log_mode)
+        self.log_level: int = log_level
+        self.log_mode: int = log_mode
 
-    def parse_log_level(self, log_level: str) -> str:
-        """
-        Parses the log_level entered into the config.json
-        """
-        log_level: str = log_level.upper()
-        log_level: str = log_level.strip()
-
-        # function returns DEBUG as the log_level if an invalid log_level is entered
-        if log_level not in self.levels_map:
-            log_level: str = "DEBUG"
-
-        return log_level
-
-    def parse_log_mode(self, log_mode: str) -> str:
-        """
-        Parses the log_mode entered into the config.json
-        """
-        log_mode: str = log_mode.upper()
-        log_mode: str = log_mode.strip()
-
-        # function returns PRINT as the log_mode if an invalid mode is entered
-        if log_mode not in self.log_modes_set:
-            log_mode: str = "PRINT"
-
-        return log_mode
-
-    def can_print_this_level(self, level: str) -> bool:
-        return self.levels_map[level] >= self.levels_map[self.log_level]
+    def can_print_this_level(self, level: int) -> bool:
+        return level >= self.log_level
 
     def _log(self, level: str, message: str, **kwargs) -> None:
         """
@@ -82,7 +64,7 @@ class Logger:
 
         json_output = json.dumps(kwargs)
 
-        if self.logToStandardOut and self.can_print_this_level(level):
+        if self.logToStandardOut and self.can_print_this_level(LogLevel.level):
             print(json_output)
 
     def debug(self, message: str, **kwargs) -> None:
