7906f4fe6862f292d521e03a51ee060ee3fda06d
blakejameson <lpo13@txstate.edu>
2025-01-28 22:50:01 -0600

    made adjustments to _log to allow for level value to be passed

--- lib/pysquared/logger.py
+++ lib/pysquared/logger.py
@@ -7,8 +7,6 @@ Logs can be output to standard output or saved to a file (functionality to be im
 import json
 import time
 
-from adafruit_blinka import Enum
-
 from lib.pysquared.nvm.counter import Counter
 from lib.pysquared.nvm.registers import ERRORCNT
 
@@ -18,13 +16,13 @@ except ImportError:
     pass
 
 
-class LogMode(Enum):
+class LogMode:
     PRINT = 1
     FILE = 2
     BOTH = 3
 
 
-class LogLevel(Enum):
+class LogLevel:
     NOTSET = 0
     DEBUG = 1
     INFO = 2
@@ -48,10 +46,10 @@ class Logger:
         self.log_level: int = log_level
         self.log_mode: int = log_mode
 
-    def can_print_this_level(self, level: int) -> bool:
-        return level >= self.log_level
+    def can_print_this_level(self, level_value: int) -> bool:
+        return level_value >= self.log_level
 
-    def _log(self, level: str, message: str, **kwargs) -> None:
+    def _log(self, level: str, level_value: int, message: str, **kwargs) -> None:
         """
         Log a message with a given severity level and any addional key/values.
         """
@@ -64,39 +62,39 @@ class Logger:
 
         json_output = json.dumps(kwargs)
 
-        if self.logToStandardOut and self.can_print_this_level(LogLevel.level):
+        if self.logToStandardOut and self.can_print_this_level(level_value):
             print(json_output)
 
     def debug(self, message: str, **kwargs) -> None:
         """
         Log a message with severity level DEBUG.
         """
-        self._log("DEBUG", message, **kwargs)
+        self._log("DEBUG", 1, message, **kwargs)
 
     def info(self, message: str, **kwargs) -> None:
         """
         Log a message with severity level INFO.
         """
-        self._log("INFO", message, **kwargs)
+        self._log("INFO", 2, message, **kwargs)
 
     def warning(self, message: str, **kwargs) -> None:
         """
         Log a message with severity level WARNING.
         """
-        self._log("WARNING", message, **kwargs)
+        self._log("WARNING", 3, message, **kwargs)
 
     def error(self, message: str, **kwargs) -> None:
         """
         Log a message with severity level ERROR.
         """
         self.error_count.increment()
-        self._log("ERROR", message, **kwargs)
+        self._log("ERROR", 4, message, **kwargs)
 
     def critical(self, message: str, **kwargs) -> None:
         """
         Log a message with severity level CRITICAL.
         """
-        self._log("CRITICAL", message, **kwargs)
+        self._log("CRITICAL", 5, message, **kwargs)
 
     def get_error_count(self) -> int:
         return self.error_count
