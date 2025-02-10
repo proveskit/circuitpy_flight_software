4221ee1345601fc6e77ee34c92fa326a81a84581
blakejameson <lpo13@txstate.edu>
2025-02-09 22:00:49 -0600

    Added docstrings and removed Config object, as it is currently not used in SleepHelper

--- lib/pysquared/sleep_helper.py
+++ lib/pysquared/sleep_helper.py
@@ -4,7 +4,6 @@ import time
 import alarm
 import digitalio
 
-from lib.pysquared.config import Config
 from lib.pysquared.logger import Logger
 from lib.pysquared.pysquared import Satellite
 
@@ -17,12 +16,31 @@ except Exception:
 
 
 class SleepHelper:
-    def __init__(self, cubesat: Satellite, logger: Logger, config: Config):
+    """
+    Class responsible for sleeping the Satellite to conserve power
+    """
+
+    def __init__(self, cubesat: Satellite, logger: Logger):
+        """
+        Creates a SleepHelper object.
+
+        :param cubesat: The Satellite object
+        :param logger: The Logger object allowing for log output
+
+        """
         self.cubesat: Satellite = cubesat
         self.logger: Logger = logger
-        self.config: Config = config
 
     def safe_sleep(self, duration: int = 15) -> None:
+        """
+        Puts the Satellite to sleep for specified duration, in seconds.
+
+        Current implementation results in an actual sleep duration that is a multiple of 15.
+        Current implementation only allows for a maximum sleep duration of 180 seconds.
+
+        :param duration: Specified time, in seconds, to sleep the Satellite for
+        """
+
         self.logger.info("Setting Safe Sleep Mode")
 
         iterations: int = 0
@@ -39,6 +57,8 @@ class SleepHelper:
             self.cubesat.watchdog_pet()
 
     def short_hibernate(self) -> Literal[True]:
+        """Puts the Satellite to sleep for 120 seconds"""
+
         self.logger.debug("Short Hibernation Coming UP")
         gc.collect()
         # all should be off from cubesat powermode
@@ -62,6 +82,8 @@ class SleepHelper:
         return True
 
     def long_hibernate(self) -> Literal[True]:
+        """Puts the Satellite to sleep for 180 seconds"""
+
         self.logger.debug("LONG Hibernation Coming UP")
         gc.collect()
         # all should be off from cubesat powermode
