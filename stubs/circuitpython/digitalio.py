0d85e83852147f4568453d98ad3246818d38ddd4
Nate Gay <email@nategay.me>
2025-02-09 23:49:24 -0600

    Add stub for CircuitPython digitalio module

---
+++ stubs/circuitpython/digitalio.py
@@ -0,0 +1,47 @@
+"""
+Stub for Circuit Python digitalio
+https://docs.circuitpython.org/en/latest/shared-bindings/digitalio/index.html
+"""
+
+from __future__ import annotations
+
+from typing import Optional
+
+import stubs.circuitpython.microcontroller as microcontroller
+from typing_extensions import Protocol
+
+
+class DriveMode(Protocol):
+    def __init__(self) -> None: ...
+    
+    PUSH_PULL: DriveMode
+    OPEN_DRAIN: DriveMode
+
+class DigitalInOut(Protocol):
+    def __init__(self, pin: microcontroller.Pin) -> None: ...
+    
+    def deinit(self) -> None: ...
+    
+    def __enter__(self) -> DigitalInOut: ...
+    
+    def __exit__(self) -> None: ...
+    
+    def switch_to_output(self, value: bool = False, drive_mode: DriveMode = DriveMode.PUSH_PULL) -> None: ...
+    
+    def switch_to_input(self, pull: Optional[Pull] = None) -> None: ...
+        
+    direction: Direction
+    value: bool
+    drive_mode: DriveMode
+    pull: Optional[Pull]
+
+
+class Direction(Protocol):
+    def __init__(self) -> None: ...
+    INPUT: Direction
+    OUTPUT: Direction
+
+class Pull(Protocol):
+    def __init__(self) -> None: ...
+    UP: Pull
+    DOWN: Pull
