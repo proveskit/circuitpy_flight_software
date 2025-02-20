1cb26c6131760e554d6ed40400f0df29a73b0cc5
JamesDCowley <jcowley04@hotmail.com>
2025-02-20 17:50:29 -0600

    Initialize StateOfHealth metrics to None and refactor update logic for improved clarity and error handling

--- lib/pysquared/state_of_health.py
+++ lib/pysquared/state_of_health.py
@@ -13,42 +13,52 @@ class StateOfHealth:
         self.c: Satellite = c
         self.logger: Logger = logger
         self.f: functions = f
-        self.state: OrderedDict[str, Any] = {}
+        self.state: OrderedDict[str, Any] = {
+            # init all values to None
+            "system_voltage": None,
+            "system_current": None,
+            "solar_voltage": None,
+            "solar_current": None,
+            "radio_temperature": None,
+            "microcontroller_temperature": None,
+            "internal_temperature": None,
+            "error_count": None,
+            "power_mode": None,
+            "battery_voltage": None,
+            "uptime": None,
+            "boot_count": None,
+            "battery_temperature": None,
+            "burned_flag": None,
+            "brownout_flag": None,
+            "fsk_flag": None,
+        }
 
     def update_state_of_health(self):
         """
         Update the state of health
         """
-        self.logger.debug("Updating State of Health")
         try:
-            self.state = {
-                # if IC20 is not enabled, we can't access this sensor, at least from what I can tell from this image: https://docs.proveskit.space/en/latest/core_documentation/hardware/images/fc_board_block.png
-                "system_voltage": self.c.micro.cpu.voltage
-                if self.c.hardware["I2C0"]
-                else None,
-                "system_current": self.c.charge_current
-                if self.c.hardware["I2C0"]
-                else None,
-                "solar_voltage": None,  # not sure how to get these values
-                "solar_current": None,
-                "radio_temperature": self.f.last_radio_temp(),
-                "microcontroller_temperature": self.c.micro.cpu.temperature,
-                "internal_temperature": None,  # self.c.internal_temperature doesn't seem to work
-                "error_count": self.logger.get_error_count(),
-                # stuff from old state_of_health function
-                "power_mode": self.c.power_mode,
-                "battery_voltage": self.c.battery_voltage
-                if self.c.hardware["I2C0"]
-                else None,
-                "uptime": self.c.get_system_uptime,
-                "boot_count": self.c.boot_count.get(),
-                "battery_temperature": self.f.last_battery_temp
-                if self.c.hardware["I2C0"]
-                else None,  # this value is never actually calculated, just retrieved from config
-                "burned_flag": self.c.f_burned.get(),
-                "brownout_flag": self.c.f_brownout.get(),
-                "fsk_flag": self.c.f_fsk.get(),
-            }
-            self.logger.info("State of Health", state=self.state)
+            # update IC20 related values
+            if self.c.hardware["I2C0"]:
+                self.state["system_voltage"] = self.c.micro.cpu.voltage
+                self.state["system_current"] = self.c.charge_current
+                self.state["solar_voltage"] = None # unsure on how to get these values
+                self.state["solar_current"] = None   
+                self.state["battery_temperature"] = self.f.last_battery_temp   
+                self.state["battery_voltage"] = self.c.battery_voltage
+            
+            self.state["radio_temperature"] = self.f.last_radio_temp()
+            self.state["microcontroller_temperature"] = self.c.micro.cpu.temperature
+            self.state["internal_temperature"] = None # self.c.internal_temperature doesn't seem to work
+            self.state["error_count"] = self.logger.get_error_count()
+            self.state["power_mode"] = self.c.power_mode
+            self.state["uptime"] = self.c.get_system_uptime
+            self.state["boot_count"] = self.c.boot_count.get()
+            self.state["burned_flag"] = self.c.f_burned.get()
+            self.state["brownout_flag"] = self.c.f_brownout.get()
+            self.state["fsk_flag"] = self.c.f_fsk.get()
+
         except Exception as e:
             self.logger.error("Couldn't acquire data for state of health", err=e)
+        
+        self.logger.info("State of Health", state=self.state)
