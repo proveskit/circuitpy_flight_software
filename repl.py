65eef345cbc478785d3d1412b216235defe5c92d
aychar <58487401+hrfarmer@users.noreply.github.com>
2025-02-21 15:08:11 -0600

    add version to repl

--- repl.py
+++ repl.py
@@ -5,10 +5,11 @@ from lib.pysquared.config import Config
 from lib.pysquared.logger import Logger
 from lib.pysquared.nvm.counter import Counter
 from lib.pysquared.pysquared import Satellite
+from version import __version__
 
 logger: Logger = Logger(
     error_counter=Counter(index=register.ERRORCNT, datastore=microcontroller.nvm)
 )
 config: Config = Config("config.json")
 logger.info("Initializing a cubesat object as `c` in the REPL...")
-c: Satellite = Satellite(config, logger)
+c: Satellite = Satellite(config, logger, __version__)
