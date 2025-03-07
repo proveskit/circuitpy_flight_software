aca7202ac8c98397a526d206d6cec82faca2f5c5
Alandlt15 <allan-1963@hotmail.com>
2025-03-06 18:06:11 -0600

    cleaning booty.py

--- boot.py
+++ boot.py
@@ -1,18 +1,14 @@
-import json
-
 import storage
 
-with open("config.json", "r") as f:
-    json_data = f.read()
-parameters = json.loads(json_data)
-if parameters["read_state"]:
-    print("read_state=true")
-    storage.enable_usb_drive()
-else:
-    print("read_state=false")
-    storage.disable_usb_drive()
+# import json
+# with open("config.json", "r") as f:
+#    json_data = f.read()
+# parameters = json.loads(json_data)
+# if parameters["read_state"]:
+#    print("read_state=true")
+#    storage.enable_usb_drive()
+# else:
+#    print("read_state=false")
+#    storage.disable_usb_drive()
 
 storage.remount("/", False)
-
-# import supervisor
-# supervisor.set_next_stack_limit(4096 + 4096)
