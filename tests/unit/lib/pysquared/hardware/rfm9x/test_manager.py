a68f6805b813983e036376e5be193b346c4b1ba1
SamuelGomezG <samgomezgu@unal.edu.co>
2025-03-17 18:38:44 -0500

    Created test in "test_manager" for "beacon_radio_message" function of radio manager class. Test was successful.

--- tests/unit/lib/pysquared/hardware/rfm9x/test_manager.py
+++ tests/unit/lib/pysquared/hardware/rfm9x/test_manager.py
@@ -116,3 +116,20 @@ def test_get_temperature(
     actual_temperature = manager.get_temperature()
     assert actual_temperature == expected_temperature
     mock_radio.read_u8.assert_called_once_with(0x5B)
+
+
+def test_beacon_radio_message(
+    mock_logger: Logger, mock_use_fsk: Flag, mock_radio_factory: MagicMock, capsys
+):
+    mock_radio = MagicMock(spec=RFMSPI)
+    mock_radio.send = MagicMock()
+    mock_radio.send.return_value = False
+    mock_radio_factory.create.return_value = mock_radio
+
+    manager = RFM9xManager(mock_logger, mock_use_fsk, mock_radio_factory)
+
+    manager.beacon_radio_message(None)
+    assert "There was an error while beaconing" in capsys.readouterr().out
+
+    manager.beacon_radio_message("Testing beaconing function in radio manager.")
+    assert "I am beaconing" in capsys.readouterr().out
